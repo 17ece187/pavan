db117e44cc41d578dbdbc237bbbf40c7c6d1530e
pavan <17ece187@gmail.com>
2023-10-03 16:06:18 +0530

    version2

--- function.js
+++ function.js
@@ -1,3 +1,7 @@
 exports.helloWorld = async (req, res) => {
+<<<<<<< HEAD
 console.log("Hello, World version 2 !");
+=======
+console.log("Hello, World rahul2 !");
+>>>>>>> parent of 521eb87 (version1)
 };
